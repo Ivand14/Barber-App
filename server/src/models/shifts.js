efffca54cddc6a047d4474e2c5542f0e12b1749d
Ivand14 <carpivan14@hotmail.com>
2023-11-10 09:48:34 -0300

    Nuevo modelo de turnos creado

---
+++ server/src/models/shifts.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize")
+
+
+module.exports = (sequelize) => {
+    sequelize.defien('shift',{
+        id:{
+            type: DataTypes.UUID,
+            allowNull:false,
+            primaryKey:true
+        },
+        cost:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        hour:{
+            type:DataTypes.ENUM('9','10','11','12','14','15','16','17','18','19','20','21','22'),
+            allowNull:false
+        },
+        day:{
+            type: DataTypes.ENUM('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'),
+            allowNull:false
+        }
+    },{timestamps:false})
+}
